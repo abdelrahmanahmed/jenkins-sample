663da30c43408d197172def55a7e2825b39d6f2e
abdelrahmanahmed <abdelrahman.wahdan@outlook.com>
2020-08-05 01:17:51 +0200

    fix: moved to vars

--- vars/common.groovy
+++ vars/common.groovy
@@ -7,7 +7,7 @@ def call() {
           script{
                if (env.BRANCH_NAME.equals('master') ){
                    
-               echo "hello master"
+               echo "hello master from common file"
             }
             else if(env.BRANCH_NAME.equals('develop')){
               echo "hello develop"
