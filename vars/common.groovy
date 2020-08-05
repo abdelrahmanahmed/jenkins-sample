c216891ef78cd436df6b7eb47fe0c5b49c21542e
abdelrahmanahmed <abdelrahman.wahdan@outlook.com>
2020-08-05 02:31:47 +0200

    fix: moved to vars

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,7 +1,12 @@
 def call() {
  node {
         stage("Install Stage from common file") {
-            echo "npm install from common files"
+             if (env.BRANCH_NAME.equals('master')){
+                   echo "npm install from common files master branch"
+             }
+             else if(env.BRANCH_NAME.equals('develop')){
+                   echo "npm install from common files develop branch"
+             }
         }
         stage("Test") {
             echo "npm test from common files"
